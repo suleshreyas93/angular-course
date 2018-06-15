2103acb0c975bfcf779535a3aac909aff4f70a1c
Shreyas Sule <suleshreyas7@gmail.com>
2018-06-15 12:18:50 -0400

    used ngFor directive to display list of courses for the course component

--- angular-fundamentals/src/app/courses.component.ts
+++ angular-fundamentals/src/app/courses.component.ts
@@ -3,11 +3,19 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'courses',
-    template:'<h2>{{ "Title: "+getTitle() }}</h2>'
+    template:`
+            <h2>{{ getTitle() }}</h2>
+            <ul>
+                <li *ngFor = "let course of courses">
+                    <h4>{{ course }}</h4>
+                </li>
+            </ul>
+        `
 })
 export class CoursesComponent
 {
     title = 'List of courses';
+    courses = ['Angular','Spring MVC','ReactJS','DS Algo'];
 
     getTitle()
     {

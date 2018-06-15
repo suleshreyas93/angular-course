cb2946ea85a08e8d5a723db2568c87c5df6866b9
Shreyas Sule <suleshreyas7@gmail.com>
2018-06-15 13:23:00 -0400

    added bootstrap

--- data-event-handling/src/app/courses.component.ts
+++ data-event-handling/src/app/courses.component.ts
@@ -11,6 +11,8 @@ import { Component } from '@angular/core';
                 <td [attr.colspan] = "colSpan"></td>
             </tr>
         </table>
+
+        <button class = "btn btn-primary">Save</button>
     `
 })
 export class CoursesComponent

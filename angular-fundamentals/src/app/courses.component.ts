be83cc97ee94195ca1561300230166ad7d6b746c
Shreyas Sule <suleshreyas7@gmail.com>
2018-06-15 12:11:47 -0400

    created template for course component to display title dynamically

--- angular-fundamentals/src/app/courses.component.ts
+++ angular-fundamentals/src/app/courses.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'courses',
-    template:'<h2>{{ title }}</h2>'
+    template:'<h2>{{ "Title: "+getTitle() }}</h2>'
 })
 export class CoursesComponent
 {
-    title: 'List of courses';
+    title = 'List of courses';
+
+    getTitle()
+    {
+        return this.title;
+    }
 }

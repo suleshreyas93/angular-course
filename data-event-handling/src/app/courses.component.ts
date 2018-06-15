7c36c4e3f0426264434203077f1e753ce4baa054
Shreyas Sule <suleshreyas7@gmail.com>
2018-06-15 13:13:07 -0400

    attribute binding

--- data-event-handling/src/app/courses.component.ts
+++ data-event-handling/src/app/courses.component.ts
@@ -5,9 +5,16 @@ import { Component } from '@angular/core';
     template: `
         <h2>{{ title }}</h2>
         <h2 [textContent] = "title"></h2>
+
+        <table>
+            <tr>
+                <td [attr.colspan] = "colSpan"></td>
+            </tr>
+        </table>
     `
 })
 export class CoursesComponent
 {
     title = "List of courses";
+    colSpan = 2;
 }

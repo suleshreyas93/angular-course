041f4730816beb0d7dcbfedf9b4a2548db4e8206
Shreyas Sule <suleshreyas7@gmail.com>
2018-06-16 19:59:42 -0400

    implemented the favorite component icon behavior

--- data-event-handling/src/app/app.module.ts
+++ data-event-handling/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { SummaryPipe } from './summary.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    SummaryPipe
+    SummaryPipe,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
---
+++ data-event-handling/src/app/favorite/favorite.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent implements OnInit {
+
+  isFavorite = true;
+
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick()
+  {
+    this.isFavorite = !this.isFavorite;
+  }
+
+}

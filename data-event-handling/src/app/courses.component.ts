f00c3be9924a4f1f4c30746e1a3e68359dd1518f
Shreyas Sule <suleshreyas7@gmail.com>
2018-06-16 17:36:18 -0400

    event handling and template variables

--- data-event-handling/src/app/courses.component.ts
+++ data-event-handling/src/app/courses.component.ts
@@ -12,11 +12,32 @@ import { Component } from '@angular/core';
             </tr>
         </table>
 
-        <button class = "btn btn-primary">Save</button>
+        <div (click) = "onDivClick()">
+            <button class = "btn btn-primary" (click) = "onSave($event)">Save</button>
+        </div>
+
+        <input (keyup.enter) = "onKeyUp($event)"/>
+        <input #email (keyup.enter) = "onKeyUp(email.value)"/>
+        
     `
 })
 export class CoursesComponent
 {
     title = "List of courses";
     colSpan = 2;
+
+    onDivClick()
+    {
+        console.log("Div clicked!");
+    }
+    onSave($event)
+    {
+        $event.stopPropagation();
+        console.log("Button was clicked!",$event);
+    }
+
+    onKeyUp(email)
+    {
+        console.log(email);
+    }
 }

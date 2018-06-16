2c60c81fc5663172b85f09b238e6744537cd9155
Shreyas Sule <suleshreyas7@gmail.com>
2018-06-16 17:47:29 -0400

    two way binding with ngModel

--- data-event-handling/src/app/courses.component.ts
+++ data-event-handling/src/app/courses.component.ts
@@ -17,12 +17,13 @@ import { Component } from '@angular/core';
         </div>
 
         <input (keyup.enter) = "onKeyUp($event)"/>
-        <input #email (keyup.enter) = "onKeyUp(email.value)"/>
+        <input [(ngModel)] = "email" (keyup.enter) = "onKeyUp()"/>
         
     `
 })
 export class CoursesComponent
 {
+    email;
     title = "List of courses";
     colSpan = 2;
 
@@ -36,8 +37,8 @@ export class CoursesComponent
         console.log("Button was clicked!",$event);
     }
 
-    onKeyUp(email)
+    onKeyUp()
     {
-        console.log(email);
+        console.log(this.email);
     }
 }

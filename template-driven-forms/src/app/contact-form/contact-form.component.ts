55aa3cdefb45afa9ca57420304927ef418bab025
Shreyas Sule <suleshreyas7@gmail.com>
2018-06-19 19:58:40 -0400

    validating inputs with specific errors

--- template-driven-forms/src/app/contact-form/contact-form.component.ts
+++ template-driven-forms/src/app/contact-form/contact-form.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
+  log(x)
+  {
+    console.log(x);
   }
 
 }
